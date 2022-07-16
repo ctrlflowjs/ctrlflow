baa33a58a97b8db2e873e83f9bf6634b26e8c573
Max North <maxnorth@hey.com>
2022-07-16 15:55:09 -0400

    local demo link to github

--- dashboard/demo/src/main.ts
+++ dashboard/demo/src/main.ts
@@ -2,3 +2,5 @@ import bootstrap from "../../src/bootstrap";
 import "./mock-actions"
 
 bootstrap()
+
+document.querySelector(".nav-bar").appendChild(document.getElementById("example-app-code-link"))
