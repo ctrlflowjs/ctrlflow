55df6cca5de726708f7791413d4b4f7eb61fc087
Max North <maxnorth@hey.com>
2022-08-16 21:43:15 -0400

    More renaming in example proj

--- examples/crm/src/server.js
+++ examples/crm/src/server.js
@@ -1,15 +1,15 @@
 const express = require("express")
-const ctrlflow = require("ctrlflowjs")
-const workflowApp = require('./lowcode/app')
+const ctrlflow = require("@ctrlflow/core")
+const ctrlflowApp = require('./ctrlflow/app')
 
 const app = express()
 const port = 3000
 
-app.use(ctrlflow.express({ app: lowcodeApp }))
+app.use(ctrlflow.express({ app: ctrlflowApp }))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 // if you also want this app to run as a worker
-workflowApp.start()
+ctrlflowApp.start()
